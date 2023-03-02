0f4be020c42b81f14ad15e644a34e6e35d326834
Alcibiades Athens <alcibiades.eth@protonmail.com>
2023-03-01 17:32:59 -0700

    resolve conflict

--- src/LibDDRV.sol
+++ src/LibDDRV.sol
@@ -13,22 +13,21 @@ pragma solidity >=0.8.8;
 
 // Associated storage structures
 
-// @notice An element that can be selected from many, with likelihood
-/// of selection dictated by its weight.
-struct Element {
+struct Edge {
+    uint256 level;
     uint256 index;
-    uint256 weight;
 }
 
-struct Range {
+struct Node {
+    uint256 index;
     uint256 weight;
-    uint256[] children;
+    Edge[] children;
 }
 
 struct Level {
     uint256 weight;
     uint256 roots;
-    mapping(uint256 => Range) ranges;
+    mapping(uint256 => Node) ranges;
 }
 
 struct Forest {
@@ -84,20 +83,50 @@ library LibDDRV {
         return n;
     }
 
+    // @retrun integer ⌊lg n⌋ of x.
     function floor_ilog(uint256 x) internal pure returns (uint256) {
         return (255 - nlz(x));
     }
 
     // One issue here, is that each iteration expects a new URV
-    function bucket_rejection() internal {}
+    function bucket_rejection(Forest storage forest, uint256 level, uint256 range, uint256 urv)
+        internal
+        returns (uint256)
+    {
+        // We want to choose a child to descend to from the range.
+        // To do this, we use the bucket rejection method as described by
+        // Knuth.
 
-    function insert_element(uint256 index, Range storage range) internal {}
+        // Here we expand numbers from the URV
+        uint256 i = 1;
+        bool repeat = true;
+        Node storage range_s = forest.levels[level].ranges[range];
+        uint256 n = range_s.children.length;
+        uint256 index;
+        uint256 ilog_n = floor_ilog(n);
+        uint256 scale_down_bits = 255 - ilog_n;
+        while (repeat) {
+            uint256 expanded_urv = uint256(keccak256(abi.encode(urv, i))) >> scale_down_bits;
+            uint256 un = expanded_urv * n;
+            // = ⌊un⌋
+            index = ((expanded_urv * n) >> ilog_n) << ilog_n;
+            if (
+                (un - index)
+                    > (forest.levels[range_s.children[index].level].ranges[range_s.children[index].index].weight << ilog_n)
+            ) {
+                repeat = false;
+            }
+        }
+        return ((index >> ilog_n) + 1);
+    }
 
-    function delete_element(uint256 index, Range storage range) internal {}
+    function insert_element(uint256 index, Node storage range) internal {}
 
-    function insert_bucket(uint256 i, Range storage range) internal {}
+    function delete_element(uint256 index, Node storage range) internal {}
 
-    function delete_range(Range storage range) internal {}
+    function insert_bucket(uint256 i, Node storage range) internal {}
+
+    function delete_range(Node storage range) internal {}
 
     // Construct a level in the forest of trees
     function construct_level(Forest storage forest, uint256 level, bytes32 ptr, bytes32 head, bytes32 tail)
@@ -114,7 +143,7 @@ library LibDDRV {
         // While Qₗ ≠ ∅
         while (head != tail) {
             // Dequeue range from storage
-            Range storage range;
+            Node storage range;
             assembly {
                 range.slot := mload(tail)
                 tail := sub(word, tail)
@@ -125,7 +154,7 @@ library LibDDRV {
             // TODO(Support expanded degree bound)
             if (range.children.length > 2) {
                 // Then this range moves to the next level
-                Range storage newRange = forest.levels[level].ranges[j];
+                Node storage newRange = forest.levels[level].ranges[j];
                 enqueue_range(ptr1, head1, tail1, j, newRange);
                 insert_bucket(j, newRange);
                 delete_range(range);
@@ -152,7 +181,7 @@ library LibDDRV {
         uint256 j;
         for (i = 1; i <= n; i++) {
             j = floor_ilog(weights[i]) + 1;
-            Range storage range = forest.levels[l].ranges[j];
+            Node storage range = forest.levels[l].ranges[j];
             insert_bucket(i, range);
             enqueue_range(ptr, head, tail, j, range);
         }
@@ -165,11 +194,11 @@ library LibDDRV {
     // TODO b-factor
     // Update an element's weight in the forest of trees
     function update_element(uint256 index, uint256 newWeight, Forest storage forest) external {
-        uint l = 1;
+        uint256 l = 1;
         // Set up an in memory queue object
         (bytes32 ptr, bytes32 head, bytes32 tail) = new_queue();
 
-        Element storage elt = get_element(index);
+        Node storage elt = get_element(index);
         uint256 oldWeight = elt.weight;
 
         // update leaf/element weight
@@ -177,7 +206,7 @@ library LibDDRV {
 
         // get the current range index
         uint256 j = floor_ilog(oldWeight) + 1;
-        Range storage currentRange = forest.levels[1].ranges[j];
+        Node storage currentRange = forest.levels[1].ranges[j];
 
         if (newWeight < 2 ** (j - 1) || (2 ** j) <= newWeight) {
             // newWeight does not fall into the same parent range
@@ -185,7 +214,7 @@ library LibDDRV {
             uint256 k = floor_ilog(newWeight) + 1;
             delete_element(index, currentRange);
             currentRange.weight -= oldWeight;
-            Range storage newRange = forest.levels[1].ranges[k];
+            Node storage newRange = forest.levels[1].ranges[k];
             insert_element(index, newRange);
             newRange.weight += newWeight;
 
@@ -202,12 +231,10 @@ library LibDDRV {
         update_levels(ptr, head, tail, forest);
     }
 
-    // Generate a discretely weighted random variate given a uniform random
-    // and a preprocessed forest of trees.
-    function generate(mapping(uint256 => mapping(uint256 => Range)) storage forest, uint256 urv) external {}
-
     // TODO
-    function get_element(uint256 index) internal returns (Element storage) {revert();}
+    function get_element(uint256 index) internal returns (Node storage) {
+        revert();
+    }
 
     function new_queue() internal returns (bytes32 ptr, bytes32 head, bytes32 tail) {
         // Set up an in memory queue object
@@ -222,7 +249,7 @@ library LibDDRV {
         }
     }
 
-    function enqueue_range(bytes32 ptr, bytes32 head, bytes32 tail, uint256 j, Range storage range) internal {
+    function enqueue_range(bytes32 ptr, bytes32 head, bytes32 tail, uint256 j, Node storage range) internal {
         assembly {
             // Check if the bit j is set in the header
             if gt(shr(255, shl(sub(255, j), mload(ptr))), 0) {
@@ -240,7 +267,7 @@ library LibDDRV {
     }
 
     // Propogate upwards any changes in the the element or range weights
-    function update_levels(bytes32 ptr, bytes32 head, bytes32 tail, Forest storage forest) internal { 
+    function update_levels(bytes32 ptr, bytes32 head, bytes32 tail, Forest storage forest) internal {
         uint256 l = 1;
         while (head != tail) {
             // Set Qₗ₊₁
@@ -249,4 +276,6 @@ library LibDDRV {
             l += 1;
         }
     }
+
+    function generate(mapping(uint256 => mapping(uint256 => Node)) storage forest, uint256 seed) external {}
 }
